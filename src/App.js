ca881443456d32ba2066e9ffe27f9adbfd8b0113
Ivan Jovanović <mrtanguero@gmail.com>
2021-01-10 23:39:53 +0100

    Samo jos da se osposobi dugme i da se blokira sve kad se zavrsi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from './components/Card';
 import './App.css';
+import Scoreboard from './components/Scoreboard';
 
 function shuffle(array) {
   var currentIndex = array.length,
@@ -24,7 +25,9 @@ function shuffle(array) {
 
 export default class App extends Component {
   state = {
-    noOfMoves: 0,
+    movesLeft: 5,
+    score: 0,
+    noOfMovesPlayed: 0,
     randomArray: shuffle([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]),
     currentMove: [],
     cardsOpen: Array(12).fill(false),
@@ -34,6 +37,69 @@ export default class App extends Component {
   componentDidUpdate() {
     console.log(this.state);
   }
+
+  resetMove = () => {
+    console.log('Ušao u glavni dio resetMove() funkcije');
+    const newCardsOpen = [...this.state.cardsOpen];
+    this.state.currentMove.forEach((move) => {
+      newCardsOpen[move] = false;
+    });
+    this.setState({
+      cardsOpen: newCardsOpen,
+      currentMove: [],
+      timeout: null,
+    });
+  };
+
+  // Async zbog toga što treba da sačeka jedan setState
+  onClickHandle = async (ind) => {
+    // Ako je karta već otvorena, iskuliraj klik
+    if (this.state.cardsOpen[ind]) {
+      return;
+    }
+
+    const newCardsOpen = [...this.state.cardsOpen];
+    newCardsOpen[ind] = !this.state.cardsOpen[ind];
+
+    await this.setState({
+      noOfMovesPlayed: this.state.noOfMovesPlayed + 1,
+      cardsOpen: newCardsOpen,
+    });
+
+    // Ako je prvi potez, poništi prethodne
+    // (u slučaju da je treći potez kliknut prije isteka timeouta)
+    if (this.state.noOfMovesPlayed === 1) {
+      console.log('No of moves === 1');
+      if (this.state.timeout) {
+        clearTimeout(this.state.timeout);
+        this.resetMove();
+      }
+    }
+
+    // Evidentiraj trenutni potez radi poređenja
+    this.setState({ currentMove: [...this.state.currentMove, ind] });
+
+    if (this.state.noOfMovesPlayed === 2) {
+      this.setState({ noOfMovesPlayed: 0 });
+      if (
+        this.state.randomArray[ind] ===
+        this.state.randomArray[this.state.currentMove[0]]
+      ) {
+        this.setState({
+          score: this.state.score + 1,
+          currentMove: [],
+        });
+        if (this.state.cardsOpen.every((el) => el)) console.log('Victory!');
+      } else {
+        this.setState({
+          timeout: setTimeout(this.resetMove, 2000),
+          movesLeft: this.state.movesLeft - 1,
+        });
+
+        if (!this.state.movesLeft) console.log('Game over!');
+      }
+    }
+  };
 
   renderCardList = () => {
     return this.state.randomArray.map((num, ind) => {
@@ -50,62 +116,12 @@ export default class App extends Component {
     });
   };
 
-  resetMove = () => {
-    // Ako nije bilo poteza dotad iskuliraj
-    // TODO: trebalo bi da ovo sad ne treba, provjeri
-    // if (!this.state.currentMove.length) return;
-
-    console.log('Ušao u glavni dio resetMove() funkcije');
-    const newCardsOpen = [...this.state.cardsOpen];
-    this.state.currentMove.forEach((move) => {
-      newCardsOpen[move] = false;
-    });
-    this.setState({
-      cardsOpen: newCardsOpen,
-      currentMove: [],
-      timeout: null,
-    });
-  };
-
-  onClickHandle = async (ind) => {
-    // Ako je karta već otvorena, iskuliraj klik
-    if (this.state.cardsOpen[ind]) {
-      return;
-    }
-    const newCardsOpen = [...this.state.cardsOpen];
-    newCardsOpen[ind] = !this.state.cardsOpen[ind];
-    await this.setState({
-      noOfMoves: this.state.noOfMoves + 1,
-      cardsOpen: newCardsOpen,
-    });
-
-    // Ako je prvi potez, poništi prethodne
-    // (u slučaju da je treći potez kliknut prije isteka timeouta)
-    if (this.state.noOfMoves === 1) {
-      console.log('No of moves === 1');
-      if (!this.state.timeout) {
-        clearTimeout(this.state.timeout);
-        this.resetMove();
-      }
-    }
-
-    // Evidentiraj trenutni potez radi poređenja
-    this.setState({ currentMove: [...this.state.currentMove, ind] });
-
-    if (this.state.noOfMoves === 2) {
-      this.setState({ noOfMoves: 0 });
-      if (
-        this.state.randomArray[ind] ===
-        this.state.randomArray[this.state.currentMove[0]]
-      ) {
-        this.setState({ currentMove: [] });
-      } else {
-        this.setState({ timeout: setTimeout(this.resetMove, 2000) });
-      }
-    }
-  };
-
   render() {
-    return <div className="app">{this.renderCardList()}</div>;
+    return (
+      <div className="app">
+        <Scoreboard score={this.state.score} movesLeft={this.state.movesLeft} />
+        {this.renderCardList()}
+      </div>
+    );
   }
 }
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Scoreboard.css';
+
+export default function Scoreboard({ score, movesLeft }) {
+  let message = 'REACT MEMORY GAME';
+  if (!movesLeft) message = 'Awwww, you failed 😢️';
+  if (score === 6) message = 'Grejt skses! 👏️👏️👏️';
+
+  return (
+    <div className="scoreboard">
+      <div className="moves">
+        <h3>
+          Moves
+          <span>{movesLeft}</span>
+        </h3>
+      </div>
+      <div className="message">
+        <h2>{message}</h2>
+        {message !== 'REACT MEMORY GAME' ? (
+          <button className="btn">TRY AGAIN</button>
+        ) : null}
+      </div>
+      <div className="score">
+        <h3>
+          Score
+          <span>{score}</span>
+        </h3>
+      </div>
+    </div>
+  );
+}
